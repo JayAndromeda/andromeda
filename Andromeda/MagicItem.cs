1ceade74a6119dc6222cb6602068766b71721d7e
JayAndromeda <nnja.jaay@gmail.com>
2021-09-22 00:11:55 -0400

    init commit

---
+++ Andromeda/MagicItem.cs
@@ -0,0 +1,7 @@
+public class MagicItem{
+    public string Name { get; set; }
+    public string Rarity { get; set; }
+    public string Type { get; set; }
+    public string Attunement { get; set; }
+    public string page { get; set; }
+}
---
+++ Andromeda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Andromeda
+{
+    class NewBaseType
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string filePath = @"C:\Users\Jennifer\source\repos\Andromeda\magicItems.json";
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string json = reader.ReadToEnd();
+                List<MagicItem> items = JsonConvert.DeserializeObject<List<MagicItem>>(json);
+            }
+        }
+    }
+}
