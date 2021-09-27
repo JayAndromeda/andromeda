ff4036e952161b58cdb7b14dd288a32f6cbd83f8
JayAndromeda <nnja.jaay@gmail.com>
2021-09-27 10:32:24 -0400

    init testing project

---
+++ AndromedaTests/AndromedaTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AndromedaTests
+{
+    [TestClass]
+    public class AndromedaTests
+    {
+        [TestMethod]
+        public void Canary()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
